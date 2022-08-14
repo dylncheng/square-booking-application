d7d5f1568f02900f1c08e20283dddea13c9dccfb
Dylan Cheng <dylan.cheng@mail.utoronto.ca>
2022-08-14 17:26:04 -0400

    added business analytics and settings page

---
+++ pages/business/analytics/index.js
@@ -0,0 +1,9 @@
+import { ThemeProvider } from '@mui/material/styles';
+
+export default function BusinessLanding() {
+    return(
+        <ThemeProvider>
+            <h1>This is the business analytics page</h1>
+        </ThemeProvider>
+    );
+}
---
+++ pages/business/settings/index.js
@@ -0,0 +1,9 @@
+import { ThemeProvider } from '@mui/material/styles';
+
+export default function BusinessLanding() {
+    return(
+        <ThemeProvider>
+            <h1>This is the business settings page</h1>
+        </ThemeProvider>
+    );
+}
